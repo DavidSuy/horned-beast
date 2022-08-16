127cf1bce3b7348856eb8089354d8bf8a7bc6ead
David Suy <DavidSuyDev@gmail.com>
2022-08-15 17:36:46 -0700

    lab01

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; David Suy</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
